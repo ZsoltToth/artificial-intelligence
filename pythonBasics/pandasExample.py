b8e53a47d9a1df86c519a898376d0e7679d14b4d
Zsolt Toth <tothzs87@gmail.com>
2019-02-27 12:58:35 +0100

    pandas task has been added

---
+++ pythonBasics/pandasExample.py
@@ -0,0 +1,14 @@
+import  pandas as pd
+
+df = pd.read_csv('data/dataset.csv')
+'''
+1) Records where the product is not waste (there is no error)
+2) How many waste are in the dataset?
+3) What is the expected Length of a product?
+4) What is the variance of the Width?
+5) Draw the histogram of Weight, Length, Width, Height. 
+6) Does these attributes follow normal distribution?
+7) Test whether they follow normal distribution or not with Kolmogorov-Smirov Test
+8) Calculate their correlation matrix.
+ 
+'''
