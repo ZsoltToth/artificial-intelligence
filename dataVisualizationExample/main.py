3a92259a08d0712ebee21f3fe3347dd8eade39b2
Zsolt Toth <tothzs87@gmail.com>
2019-03-06 13:24:52 +0100

    data visualization example added
    Excel has been read and queried. A line chart for Hungary has been generated.

--- dataVisualizationExample/main.py
+++ dataVisualizationExample/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 countriesMetadata = pd.read_excel(
     'data/worldbankPopulation1564.xls',
@@ -8,3 +10,26 @@ population = pd.read_excel(
     'data/worldbankPopulation1564.xls',
     sheet_name='Data',
     skiprows=[0,1,2])
+
+
+'''
+Create a line chart which shows the population changes of Hungary between 1960 and 2017
+'''
+
+years = np.arange(start=1960, stop=2018,step=1)
+
+hunPopulation = [population[population['Country Code'] == 'HUN']
+                 [str(y)].values[0] for y in years]
+
+plt.plot(years, hunPopulation)
+plt.show()
+
+'''
+Task
+Generate a line chart for each country. 
+Title should be filled properly.
+
+Bonus task:
+Title should contain the Region.
+'''
+
