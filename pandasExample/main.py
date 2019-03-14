07676c1503ddddbecf00bb7b76a49c9f77c6f43b
Zsolt Toth <tothzs87@gmail.com>
2019-03-14 20:02:18 +0100

    pandas example added
    Pandas Example project contain the reading of some data sets. It shows examples for querying and filtering data frames.

---
+++ pandasExample/main.py
@@ -0,0 +1,103 @@
+'''
+data folder contains some data sets downloaded from https://data.worldbank.org/indicator
+
+Data Sets are related to Science and Technology.
+
+Data sets are the following:
+ - Charges for the use of intellectual property, payments (BoP, current US$)
+ - High-technology exports (% of manufactured exports)
+ - Research and development expenditure (% of GDP)
+ - Scientific and technical journal articles
+ - Researchers in R&D (per million people)
+ - Technicians in R&D (per million people)
+
+
+
+'''
+
+import pandas as pd
+
+#xlrd package has to be installed because we are reading Excel files.
+charges = pd.read_excel('data/chargesForUseOfIntellectualProperty.xls',
+                        sheet_name='Data',
+                        skiprows=[0,1,2])
+
+HTExport = pd.read_excel('data/highTechExport.xls',
+                        sheet_name='Data',
+                        skiprows=[0,1,2])
+
+journalArticles = pd.read_excel('data/journalArticles.xls',
+                        sheet_name='Data',
+                        skiprows=[0,1,2])
+
+expenditures = pd.read_excel('data/RDExpenditure.xls',
+                        sheet_name='Data',
+                        skiprows=[0,1,2])
+
+researchers = pd.read_excel('data/researchersInRD.xls',
+                        sheet_name='Data',
+                        skiprows=[0,1,2])
+
+technicians = pd.read_excel('data/techniciansInRD.xls',
+                        sheet_name='Data',
+                        skiprows=[0,1,2])
+'''
+We can browse the content of these dataframes in PyCharm. 
+Special Variables -> "View as DataFrame"
+
+We can see that most of the data is missing.
+Missing values are denoted by NaN and they can be counted by the isnull and sum functions.
+The following line calculates the ratio of the missing values in the expenditures data set.
+
+expenditures.isnull().sum().sum() / expenditures.size
+
+Due to the high missing ratio, we narrow our search after 2005.
+In addition, we drop the records which contain missing data in any year. 
+'''
+
+SELECTED_PERIOD = [
+    'Country Name',
+    'Country Code',
+    '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'
+    ]
+
+
+
+charges = charges[SELECTED_PERIOD].dropna()
+HTExport = HTExport[SELECTED_PERIOD].dropna()
+journalArticles = journalArticles[SELECTED_PERIOD].dropna()
+expenditures = expenditures[SELECTED_PERIOD].dropna()
+researchers = researchers[SELECTED_PERIOD].dropna()
+technicians = technicians[SELECTED_PERIOD].dropna()
+'''
+Because the technicians data frame does not count much records after reduction, we considers only the other data sets.
+'''
+del technicians
+
+countryCodes = set(charges['Country Code']).intersection(
+    HTExport['Country Code']).intersection(
+    journalArticles['Country Code']).intersection(
+    expenditures['Country Code']).intersection(
+    researchers['Country Code'])
+#The following Country Codes denote areas so they are removed
+countryCodes.remove('EAS')
+countryCodes.remove('ECS')
+countryCodes.remove('EMU')
+countryCodes.remove('LTE')
+
+'''
+At this point we have 5 data sets (indicators) which contains time series (2005-2016) about 26 countries (countryCodes).
+There is a choice now. 
+a) We can analyze each indicator separately.
+b) We can analyze the countries by the indicators.  
+
+This example focuses on pandas so lets continue with option b.
+
+So lets query the dataset for a single country (GBR) and merge these indicators into a single data set.
+
+Expected result
+Year Charges Export Expenditures Journals Researchers
+2005
+2006
+etc.
+'''
