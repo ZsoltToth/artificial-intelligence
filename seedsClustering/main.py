0995930ee1d92ec8a33b52dc000bd53b125ee4b5
Zsolt Toth <tothzs87@gmail.com>
2019-04-03 12:24:10 +0200

    seeds clustering examples is added
    seeds dataset is added and read with pandas

---
+++ seedsClustering/main.py
@@ -0,0 +1,25 @@
+'''
+Clustering example over the Seeds Dataset from UCI Machine Learning Repo.
+
+http://archive.ics.uci.edu/ml/datasets/seeds
+
+1. area A,
+2. perimeter P,
+3. compactness C = 4*pi*A/P^2,
+4. length of kernel,
+5. width of kernel,
+6. asymmetry coefficient
+7. length of kernel groove.
+'''
+
+import pandas as pd
+
+seeds = pd.read_csv('data/seeds_dataset.txt',
+                    sep='\t',
+                    names=['area',
+                           'perimeter',
+                           'compactness',
+                           'kernelLength',
+                           'kernelWidth',
+                           'asymmetryCoeff',
+                           'kernelGroove'])
